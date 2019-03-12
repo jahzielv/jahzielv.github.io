9568ee76b7beb8b809fcd305ca4bfebe5de1cc5b
Jahziel Villasana <jahzielv@gmail.com>
2019-03-12 10:19:16 -0400

    fixed error in index.js

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ if (isDark === null) {
 if (isDark == 1) {
     document.getElementsByTagName("body")[0].style.backgroundColor = "#002b36";
     document.getElementsByClassName("nav-menu")[0].style.color = "#839496";
-    document.getElementsByClassName("main-content").forEach(element => {
-        element.style.color = "#839496";
-    });
+    Array.prototype.forEach.call(
+        document.getElementsByClassName("main-content"),
+        element => {
+            element.style.color = "#839496";
+        }
+    );
     document.getElementById("theme-toggle").innerHTML = "🌝";
     document.getElementById("syntax-highlight").setAttribute("href", "/dark.css");
 } else {
     document.getElementsByTagName("body")[0].style.backgroundColor = "#fdf6e3";
     document.getElementsByClassName("nav-menu")[0].style.color = "#073642";
-    document.getElementsByClassName("main-content").forEach(element => {
-        element.style.color = "#073642";
-    });
+    Array.prototype.forEach.call(
+        document.getElementsByClassName("main-content"),
+        element => {
+            element.style.color = "#073642";
+        }
+    );
     document.getElementById("theme-toggle").innerHTML = "🌚";
     document.getElementById("syntax-highlight").setAttribute("href", "/light.css");
 }
@@ -24,19 +30,25 @@ function themeToggleClick() {
     if (isDark == 1) {
         document.getElementsByTagName("body")[0].style.backgroundColor = "#fdf6e3";
         document.getElementsByClassName("nav-menu")[0].style.color = "#073642";
-        document.getElementsByClassName("main-content").forEach(element => {
-            element.style.color = "#073642";
-        });
         document.getElementById("theme-toggle").innerHTML = "🌚";
+        Array.prototype.forEach.call(
+            document.getElementsByClassName("main-content"),
+            element => {
+                element.style.color = "#073642";
+            }
+        );
         document.getElementById("syntax-highlight").setAttribute("href", "/light.css");
         isDark = 0;
         localStorage.setItem("isDark", isDark);
     } else {
         document.getElementsByTagName("body")[0].style.backgroundColor = "#002b36";
         document.getElementsByClassName("nav-menu")[0].style.color = "#839496";
-        document.getElementsByClassName("main-content").forEach(element => {
-            element.style.color = "#839496";
-        });
+        Array.prototype.forEach.call(
+            document.getElementsByClassName("main-content"),
+            element => {
+                element.style.color = "#839496";
+            }
+        );
         document.getElementById("theme-toggle").innerHTML = "🌝";
         document.getElementById("syntax-highlight").setAttribute("href", "/dark.css");
         isDark = 1;
