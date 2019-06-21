34ce8839938e793a54b67a62c6571b04c24c244b
Jahziel Villasana <jahzielv@gmail.com>
2019-06-21 15:04:58 -0600

    trying a fix for white flash

---
+++ _drafts/summer-update.md
@@ -0,0 +1,15 @@
+---
+layout: post
+comments: true
+title: Update - Summer Edition
+---
+
+# Update - Summer Edition
+
+Good morning blog world! I'm delivering this very late report on my summer straight from the airport as I wait for my flight.
+
+## New Job! 👨‍💻
+
+The primary reason for my absence was the start of my new summer intership at State Farm. I was brought on as a Software Development intern working on the APIs and infrastructure behind State Farm's claims operations. I was really excited to start my first real software engineering job, and have learned quite a bit about what it's like to be an enterprise software developer.
+
+## Hello
--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-let isDark = localStorage.getItem("isDark");
-if (isDark === null) {
-    isDark = 0;
-    localStorage.setItem("isDark", 0);
-}
-if (isDark == 1) {
-    document.getElementsByTagName("body")[0].style.backgroundColor = "#002b36";
-    document.getElementsByClassName("nav-menu")[0].style.color = "#839496";
-    Array.prototype.forEach.call(
-        document.getElementsByClassName("main-content"),
-        element => {
-            element.style.color = "#839496";
-        }
-    );
-    document.getElementById("theme-toggle").innerHTML = "🌝";
-    document.getElementById("syntax-highlight").setAttribute("href", "/dark.css");
-} else {
-    document.getElementsByTagName("body")[0].style.backgroundColor = "#fdf6e3";
-    document.getElementsByClassName("nav-menu")[0].style.color = "#073642";
-    Array.prototype.forEach.call(
-        document.getElementsByClassName("main-content"),
-        element => {
-            element.style.color = "#073642";
-        }
-    );
-    document.getElementById("theme-toggle").innerHTML = "🌚";
-    document.getElementById("syntax-highlight").setAttribute("href", "/light.css");
-}
 function themeToggleClick() {
     if (isDark == 1) {
         document.getElementsByTagName("body")[0].style.backgroundColor = "#fdf6e3";
