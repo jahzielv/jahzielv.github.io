faabfdf5d598e99119a1c89f7b65ba7c8995d2e1
Jahziel Villasana <jahzielv@gmail.com>
2019-06-21 17:50:44 -0600

    better styling management!

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 function themeToggleClick() {
-    if (isDark == 1) {
-        document.getElementsByTagName("body")[0].style.backgroundColor = "#fdf6e3";
-        document.getElementsByClassName("nav-menu")[0].style.color = "#073642";
-        document.getElementById("theme-toggle").innerHTML = "🌚";
-        Array.prototype.forEach.call(
-            document.getElementsByClassName("main-content"),
-            element => {
-                element.style.color = "#073642";
-            }
-        );
-        document.getElementById("syntax-highlight").setAttribute("href", "/light.css");
-        isDark = 0;
-        localStorage.setItem("isDark", isDark);
+    let light = document.getElementById("light");
+    if (light.disabled == true) {
+        light.disabled = false;
+        localStorage.removeItem("theme");
+        document.getElementById("themeToggle").innerHTML = "🌚";
+        twemoji.parse(document.getElementById("themeToggle"));
     } else {
-        document.getElementsByTagName("body")[0].style.backgroundColor = "#002b36";
-        document.getElementsByClassName("nav-menu")[0].style.color = "#839496";
-        Array.prototype.forEach.call(
-            document.getElementsByClassName("main-content"),
-            element => {
-                element.style.color = "#839496";
-            }
-        );
-        document.getElementById("theme-toggle").innerHTML = "🌝";
-        document.getElementById("syntax-highlight").setAttribute("href", "/dark.css");
-        isDark = 1;
-        localStorage.setItem("isDark", isDark);
+        light.disabled = true;
+        localStorage.setItem("theme", "dark");
+        document.getElementById("themeToggle").innerHTML = "🌝";
+        twemoji.parse(document.getElementById("themeToggle"));
     }
 }
